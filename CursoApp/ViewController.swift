c96bff39566e64a249c80d2c5816e7495b3df69f
Oscar Gonzalez <oscargonzalez159@gmail.com>
2022-10-10 11:27:17 -0300

    fix linea window

--- CursoApp/ViewController.swift
+++ CursoApp/ViewController.swift
@@ -9,8 +9,6 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var window:UIWindow?
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
